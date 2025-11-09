5ad064c232b320fdebcea3741a4bb4e6222018ed
Pourixa <pouria.footballer@gmail.com>
2025-11-09 21:25:17 +0100

    Add UI and finish the game at 5 points
    Added a simple ui and finishing the game when someone gets 5 points

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+humanScore=0
+ComputerScore=0
+
 
 function getComputerChoice()
 {
@@ -8,38 +11,44 @@ function getComputerChoice()
     else action = "Scissors"
     return action
 }
+
+let btnContainer = document.querySelector(".button-container")
+
+btnContainer.addEventListener("click",(e) => {e.preventDefault();playRound(getHumanChoice(e.target.innerText),getComputerChoice())})
+
+let ul = document.querySelector(".history")
+let usr = document.querySelector(".usr")
+let pc = document.querySelector(".pc")
+
 function getHumanChoice(choice)
 {
-    choice = prompt("Choice?")
     return choice
 }
 
 
 
 function playRound(humanChoice,computerChoice)
+
 {   
+    let li = document.createElement("li")
+    
     CC=computerChoice.toLowerCase() ;
     HC=humanChoice.toLowerCase() ;
-    if (CC===HC) console.log(`draw ${++humanScore} - ${++ComputerScore}`);
-    else if (CC==="rock" && HC === "scissors") console.log(`Computer won ${humanScore} - ${++ComputerScore}`);
-    else if (CC==="paper" && HC === "rock") console.log(`Computer won ${humanScore} - ${++ComputerScore}`);
-    else if (CC==="scissors" && HC === "paper") console.log(`Computer won ${humanScore} - ${++ComputerScore}`);
-    else console.log(`You won ${++humanScore} - ${ComputerScore}`);
-}
-
-function playGame()
-{   
-    humanScore=0
-    ComputerScore=0
-
-    for (let i=1; i<=5 ; i++) 
-    {
-        cc=getComputerChoice()
-        hc=getHumanChoice()
-
-        console.log(cc,hc)
-        console.log(playRound(hc,cc))
+    if (CC===HC) { pc.textContent = ++ComputerScore ; usr.textContent = ++humanScore ;li.textContent=(`draw ${CC} - ${HC}`);ul.appendChild(li)}
+    else if (CC==="rock" && HC === "scissors") {pc.textContent = ++ComputerScore ; usr.textContent = humanScore ;li.textContent=`Computer won ${CC} - ${HC}`;ul.appendChild(li)}
+    else if (CC==="paper" && HC === "rock") {pc.textContent = ++ComputerScore ; usr.textContent = humanScore ;li.textContent=`Computer won ${CC} - ${HC}`;ul.appendChild(li)}
+    else if (CC==="scissors" && HC === "paper") {pc.textContent = ++ComputerScore ; usr.textContent = humanScore ;li.textContent=`Computer won ${CC} - ${HC}`;ul.appendChild(li)}
+    else {pc.textContent = ComputerScore ; usr.textContent = ++humanScore ;li.textContent=`You won ${CC} - ${HC}`;ul.appendChild(li);}
+    if (humanScore>=5){
+        let body = document.querySelector("body")
+        let fin = document.createElement("h1");
+        fin.textContent = `YOU WON!!!!`;
+        body.innerHTML="";body.appendChild(fin)}
+    else if(ComputerScore>=5)
+        {
+        let body = document.querySelector("body")
+        let fin = document.createElement("h1");
+        fin.textContent = `pc won :(`;
+        body.innerHTML="", body.appendChild(fin)}
     }
-}
 
-playGame()
